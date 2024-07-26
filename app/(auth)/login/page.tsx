2c0e9cc12cf52979ba701fe44937b96a6c94ffd7
Ashim Rai <ashrai14691@gmail.com>
2024-07-26 13:08:40 +0545

    Replaced ' with &apos;

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -63,7 +63,7 @@ const Login = () => {
                 <span>Login with Google</span>
               </Button>
               <div className="flex gap-2">
-                <p>Don't have an account?</p>
+                <p>Don&apos;t have an account?</p>
                 <Link href="/register" className="text-green-600">
                   Sign Up
                 </Link>
