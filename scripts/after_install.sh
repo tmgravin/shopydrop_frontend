71bfca749e47b6cf8f537a39d441695ea87f61e3
Chyanba <98306492+tmgravin@users.noreply.github.com>
2024-09-03 16:45:20 +0545

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-# Ensure Node.js and npm are installed
-if ! command -v node &> /dev/null || ! command -v npm &> /dev/null
-then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-    source ~/.nvm/nvm.sh
-    nvm install 18
-    nvm use 18
-    nvm alias default 18
-fi
-
-# Log node and npm versions
-echo "Node version: $(node -v)"
-echo "npm version: $(npm -v)"
-
 # Navigate to the correct directory
 cd /home/ec2-user/next
-
+sudo su
 # Install dependencies
 npm install
 npm run build
